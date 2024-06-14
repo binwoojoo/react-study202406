59d25481ed35df7f6c26671bffa8d7474525f5c1
oooowb <oooo940212@gmail.com>
2024-06-14 11:45:41 +0900

    CourseGoal 앱2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React, { useState } from 'react';
-import CourseInput from './components/CourseGoals/CourseInput';
-import CourseList from './components/CourseGoals/CourseList';
-import './App.css';
+import React, { useState } from "react";
+import CourseInput from "./components/CourseGoals/CourseInput";
+import CourseList from "./components/CourseGoals/CourseList";
+import "./App.css";
+
+// 기본 더미 데이터
+const DUMMY_DATA = [
+  { id: "g1", text: "리액트 컴포넌트 스타일링 마스타하기" },
+  { id: "g2", text: "UI/UX 프로그래밍 씹쌉" },
+];
 
 const App = () => {
+
+  const [goals, setGoals] = useState(DUMMY_DATA);
+
+  
+
   return (
     <div>
       <section id="goal-form">
         <CourseInput />
       </section>
       <section id="goals">
-        <CourseList />
+        <CourseList items={goals} />
       </section>
     </div>
   );
