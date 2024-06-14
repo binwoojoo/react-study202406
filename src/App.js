18159c9868bb3397d937d6c8eab54b8d93b2bf6f
oooowb <oooo940212@gmail.com>
2024-06-14 11:35:19 +0900

    CourseGoal 앱

--- src/App.js
+++ src/App.js
@@ -1,60 +1,18 @@
-import React, { useState } from "react";
-import "./App.css";
-// import ExpenseItem from "./components/expenses/ExpenseItem";
-import ExpenseList from "./components/expenses/ExpenseList";
-import NewExpense from "./components/new-expense/NewExpense";
-// import Greet from "./components/Greet";
+import React, { useState } from 'react';
+import CourseInput from './components/CourseGoals/CourseInput';
+import CourseList from './components/CourseGoals/CourseList';
+import './App.css';
 
 const App = () => {
-  // 서버에서 지출항목 JSON 배열을 응답받음
-  const expenses = [
-    {
-      title: "치킨먹음",
-      price: 30000,
-      date: new Date(2024, 6 - 1, 3),
-    },
-    {
-      title: "족발먹음",
-      price: 40000,
-      date: new Date(2023, 12 - 1, 7),
-    },
-    {
-      title: "헬스장등록",
-      price: 300000,
-      date: new Date(2024, 6 - 1, 12),
-    },
-    {
-      title: "파파존스",
-      price: 40000,
-      date: new Date(2022, 3 - 1, 4),
-    },
-    {
-      title: "파리채",
-      price: 1500,
-      date: new Date(2023, 5 - 1, 6),
-    },
-  ];
-
-  // 배열을 상태 변수로 관리
-  const [expenseList,setExpenseList] = useState(expenses);
-
-  // ExpenseForm에게 내려보낼 함수
-  const onAddExpense = (userInput) => {
-
-    // expenseList.push(userInput);
-
-    // const newExpenseList = [...expenseList, userInput];
-
-    setExpenseList([...expenseList, userInput]);
-
-    // console.log(expenseList);
-  };
-
   return (
-    <>
-      <NewExpense onSave={onAddExpense} />
-      <ExpenseList expenses={expenseList} />
-    </>
+    <div>
+      <section id="goal-form">
+        <CourseInput />
+      </section>
+      <section id="goals">
+        <CourseList />
+      </section>
+    </div>
   );
 };
 
