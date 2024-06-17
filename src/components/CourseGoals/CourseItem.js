2531c0629206eddcd083d9e44ce345f9cb95eab9
oooowb <oooo940212@gmail.com>
2024-06-17 10:34:03 +0900

    동적 스타일 변경
    모듈.css

--- src/components/CourseGoals/CourseItem.js
+++ src/components/CourseGoals/CourseItem.js
@@ -1,14 +1,14 @@
 import React from "react";
-import "./CourseItem.css";
+import styles from "./CourseItem.module.css";
 
 const CourseItem = ({ item, onDelete }) => {
   const deleteHandler = (e) => {
     // 여기서 App.js에게 삭제 대상의 id를 전달
-    onDelete(item.id)
+    onDelete(item.id);
   };
 
   return (
-    <li className="goal-item" onClick={deleteHandler}>
+    <li className={styles["goal-item"]} onClick={deleteHandler}>
       {item.text}
     </li>
   );
