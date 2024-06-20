673570a2b4fa2041540b32e10caebfbfe69946e2
oooowb <oooo940212@gmail.com>
2024-06-20 10:30:22 +0900

    Ref 상태값 연습 템플릿

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import "./App.css";
-
-// bootstrap
-import "bootstrap/dist/css/bootstrap.min.css";
-
-import TodoTemplate from "./components/Todo/TodoTemplate";
+import React from 'react';
+import Player from './components/Ref/Player';
 
 const App = () => {
-  return <TodoTemplate />;
+
+  return (
+    <>
+      <Player />
+      <div id="challenges"></div>
+    </>
+  );
 };
 
 export default App;
