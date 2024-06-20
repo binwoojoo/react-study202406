e8da01afefd608eb92e4e96f62948001b1f497fe
oooowb <oooo940212@gmail.com>
2024-06-20 11:58:04 +0900

    Forward Refs

--- src/components/Ref/TimeChallenge.js
+++ src/components/Ref/TimeChallenge.js
@@ -1,25 +1,27 @@
-import React, { useRef, useState } from "react";
-import ResultModal from "./ResultModal";
-
-let timer;
+import React, { useRef, useState } from 'react';
+import ResultModal from './ResultModal';
 
 const TimerChallenge = ({ title, targetTime }) => {
   // timer를 ref변수로 관리
   const timer = useRef();
 
-  // 타이터가 시작되었는지를 확인하는 상태값
+  // 타이머가 시작되었는지를 확인하는 상태값
   const [timerStarted, setTimerStarted] = useState(false);
 
-  // 타겟 시간이 종료되었는지 여부
+  // 타겟시간이 종료되었는지 여부
   const [timerExpired, setTimerExpired] = useState(false);
+
+  // 자식컴포넌트 ResultModal에 있는 dialog태그의 참조를 만듬
+  const dialog = useRef();
 
   const startHandler = (e) => {
     timer.current = setTimeout(() => {
-      //   console.log("시간만료");
       setTimerExpired(true);
+      // modal open!
+      dialog.current.showModal();
     }, targetTime * 1000);
 
-    setTimerStarted(!timerStarted);
+    setTimerStarted(true);
   };
 
   const stopHandler = (e) => {
@@ -28,19 +30,19 @@ const TimerChallenge = ({ title, targetTime }) => {
 
   return (
     <>
-      {<ResultModal targetTime={targetTime} result={"lost"} />}
+      <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
       <section className="challenge">
         <h2>{title}</h2>
         <p className="challenge-time">
-          {targetTime} second{targetTime > 1 ? "s" : ""}
+          {targetTime} second{targetTime > 1 ? 's' : ''}
         </p>
         <p>
           <button onClick={timerStarted ? stopHandler : startHandler}>
-            {timerStarted ? "Stop" : "Start"} Challenge
+            {timerStarted ? 'Stop' : 'Start'} Challenge
           </button>
         </p>
-        <p className={timerStarted ? "active" : undefined}>
-          {timerStarted ? "Time is running..." : "Timer inactive"}
+        <p className={timerStarted ? 'active' : undefined}>
+          {timerStarted ? 'Time is running...' : 'Timer inactive'}
         </p>
       </section>
     </>
