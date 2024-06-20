5400f1c5caff98def7e8e940d9ec8735d619b11f
oooowb <oooo940212@gmail.com>
2024-06-20 10:20:58 +0900

    할 일 체크

--- src/components/Todo/TodoMain.js
+++ src/components/Todo/TodoMain.js
@@ -2,11 +2,16 @@ import React from "react";
 import "./scss/TodoMain.scss";
 import TodoItem from "./TodoItem";
 
-const TodoMain = ({ todos, onRemove }) => {
+const TodoMain = ({ todos, onRemove, onCheck }) => {
   return (
     <ul className="todo-list">
       {todos.map((todo) => (
-        <TodoItem key={todo.id} item={todo} onRemove={onRemove} />
+        <TodoItem
+          key={todo.id}
+          item={todo}
+          onRemove={onRemove}
+          onCheck={onCheck}
+        />
       ))}
     </ul>
   );
--- src/components/Todo/TodoTemplate.js
+++ src/components/Todo/TodoTemplate.js
@@ -1,14 +1,14 @@
-import React, { useState } from "react";
-import "./scss/TodoTemplate.scss";
-import TodoHeader from "./TodoHeader";
-import TodoInput from "./TodoInput";
-import TodoMain from "./TodoMain";
+import React, { useState } from 'react';
+import './scss/TodoTemplate.scss';
+import TodoHeader from './TodoHeader';
+import TodoInput from './TodoInput';
+import TodoMain from './TodoMain';
 
 const DUMMY_TODOS = [
-  { id: 1, title: "리액트 공부", done: true },
-  { id: 2, title: "점심 먹기", done: false },
-  { id: 3, title: "프로젝트하기", done: false },
-  { id: 4, title: "숙제하기", done: false },
+  { id: 1, title: '리액트 공부', done: true },
+  { id: 2, title: '점심 먹기', done: false },
+  { id: 3, title: '프로젝트하기', done: false },
+  { id: 4, title: '숙제하기', done: false },
 ];
 
 const TodoTemplate = () => {
@@ -28,18 +28,39 @@ const TodoTemplate = () => {
       done: false,
     };
 
-    console.log("new: ", newTodo);
+    // console.log('new: ', newTodo);
+
     setTodoList((prevTodoList) => [...prevTodoList, newTodo]);
   };
 
-  const removeTodo = id => {
-    setTodoList(todoList.filter(todo => todo.id !== id))
+  const removeTodo = (id) => {
+    setTodoList(todoList.filter((todo) => todo.id !== id));
   };
 
+  const checkTodo = (id) => {
+    // const copyTodoList = [...todoList];
+
+    // const foundTodo = copyTodoList.find(todo => todo.id === id);
+    // foundTodo.done = !foundTodo.done;
+
+    // setTodoList(copyTodoList);
+
+    setTodoList(
+      todoList.map(todo => 
+        todo.id === id 
+        ? {...todo, done: !todo.done}
+        : todo
+    ));
+  };
+  
   return (
     <div className="TodoTemplate">
       <TodoHeader />
-      <TodoMain todos={todoList} onRemove={removeTodo}/>
+      <TodoMain
+        todos={todoList}
+        onRemove={removeTodo}
+        onCheck={checkTodo}
+      />
       <TodoInput onAdd={addTodo} />
     </div>
   );
