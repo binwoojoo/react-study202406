7c97177a134e377ecbb2bac737cea872fda3a97a
oooowb <oooo940212@gmail.com>
2024-06-20 12:30:26 +0900

    점수 계산

--- src/components/Ref/ResultModal.js
+++ src/components/Ref/ResultModal.js
@@ -1,18 +1,25 @@
-import React, { forwardRef } from 'react';
+import React, { forwardRef } from "react";
 
-const ResultModalComponent = ({onReset, targetTime, remainingTime}, ref) => {
-
+const ResultModalComponent = ({ onReset, targetTime, remainingTime }, ref) => {
   // 패배조건
   const isLost = remainingTime <= 0;
 
   // 시간 포맷팅
   const formattedRemaningTime = (remainingTime / 1000).toFixed(2);
 
+  // 점수
+  const score = Math.round(1 - remainingTime / (targetTime * 1000)) * 100;
+
   return (
     <dialog ref={ref} className="result-modal">
-      {isLost && <h2>Your lost!</h2>}
-      <p>The target time was <strong>{targetTime} seconds.</strong></p>
-      <p>You stopped the timer with <strong>{formattedRemaningTime} seconds left.</strong></p>
+      {isLost ? <h2>Your lost!</h2> : <h2>Your Score: {score}!</h2>}
+      <p>
+        The target time was <strong>{targetTime} seconds.</strong>
+      </p>
+      <p>
+        You stopped the timer with{" "}
+        <strong>{formattedRemaningTime} seconds left.</strong>
+      </p>
       <form method="dialog">
         <button onClick={onReset}>Close</button>
       </form>
