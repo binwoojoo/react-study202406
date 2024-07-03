a8bb1ef81655f2003684e3f1e82429f8411ca7b0
oooowb <oooo940212@gmail.com>
2024-07-03 12:13:58 +0900

    예외 처리

--- src/components/RouteExample/pages/Events.js
+++ src/components/RouteExample/pages/Events.js
@@ -1,4 +1,3 @@
-
 import EventList from "../components/EventList";
 import { useLoaderData } from "react-router-dom";
 // import { Link } from "react-router-dom";
@@ -13,7 +12,7 @@ const Events = () => {
   //     console.log(jsonData);
   //     setEventList(jsonData);
   //   });
-
+  console.log("event page rendering$$$$$");
   return (
     <>
       <h1>Events Page</h1>
@@ -27,16 +26,27 @@ export default Events;
 // loader를 app.js로부터 아웃소싱
 
 export const loader = async () => {
+  console.log("loader call@@@@@");
+
   // 이 페이지가 열릴 때 자동으로 트리거되어 호출되는 함수
   // 이 함수에는 페이지가 열리자마자 해야할 일을 적을 수 있습니다.
 
   const response = await fetch("http://localhost:8282/events");
+
+  if (response.status !== 200) {
+    const errorText = await response.text();
+
+    throw new Response(JSON.stringify({ message: errorText }), {
+      status: response.status,
+    });
+  }
+
   // const jsonData = await response.json();
 
   // loader가 리턴한 데이터는 loader를 선언한 컴포넌트와
   // 그 하위 컴포넌트에서 언제든 불러 사용할 수 있다.
 
   // loader에서 fetch의 결과를 바로 리턴하면 알아서 json을 추출해줌.
-  
-  return response;
+
+  return response; // ok일 경우
 };
